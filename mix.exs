777d827ad4922b5d0dad20518d2a0ddfba858617
ivymarkwell <me@ivymarkwell.com>
2019-04-21 19:34:48 -0500

    add formatter and liveview

--- mix.exs
+++ mix.exs
@@ -32,12 +32,13 @@ defmodule Minesweeper.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
+      {:phoenix, "~> 1.4.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
     ]
   end
 end
