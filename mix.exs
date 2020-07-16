bc30c56c20668aabd5862a94e5f4d9f443388159
Ivy Markwell <me@ivymarkwell.com>
2020-07-16 00:36:57 -0500

    elixir 1.10.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Minesweeper.Mixfile do
     [
       app: :minesweeper,
       version: "0.0.1",
-      elixir: "~> 1.10.2",
+      elixir: "~> 1.10.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
